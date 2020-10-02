57120c0fddf0a21c6487ecfc058376b7173c3278
stbugyei <stbugyei@yahoo.co.uk>
2020-10-02 00:28:02 -0100

    Updates

---
+++ precache-manifest.00f78542085bc085329fd8fec9209563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fed43761d7fbe63d145eb207843f7db",
+    "url": "/Github-Profile/index.html"
+  },
+  {
+    "revision": "1c295b3709b887cae837",
+    "url": "/Github-Profile/static/css/main.27b35b3d.chunk.css"
+  },
+  {
+    "revision": "d04cfdc75b66c777a107",
+    "url": "/Github-Profile/static/js/2.39394fd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Github-Profile/static/js/2.39394fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c295b3709b887cae837",
+    "url": "/Github-Profile/static/js/main.75268998.chunk.js"
+  },
+  {
+    "revision": "aeebb23bebe5c0279e76",
+    "url": "/Github-Profile/static/js/runtime-main.7f7a9878.js"
+  }
+]);
